eb4cce09daaaa9b69ab5f2a0613949ec588fb167
pawan779 <pawan.dharel77@gmail.com>
2019-12-23 20:36:51 +0545

    update and delete done sucessfully

--- app/src/main/java/com/pawan/employeeapi/UpdateDeleteActivity.java
+++ app/src/main/java/com/pawan/employeeapi/UpdateDeleteActivity.java
@@ -3,13 +3,28 @@ package com.pawan.employeeapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.pawan.employeeapi.Url.Url;
+import com.pawan.employeeapi.api.EmployeeApi;
+import com.pawan.employeeapi.model.Employee;
+import com.pawan.employeeapi.model.EmployeeCUD;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UpdateDeleteActivity extends AppCompatActivity {
     private EditText etEmployeeNo,etUpdateName,etUpdateSalary,etUpdateAge;
     private Button btnSearchUpdate,btnUpdate,btnDelete;
+    Retrofit retrofit;
+    EmployeeApi employeeApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,9 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         btnSearchUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                
+                loadData();
+
 
             }
         });
@@ -34,15 +52,84 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+updateEmployee();
             }
         });
 
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+deleteEmployee();
             }
         });
+    }
+
+    private void deleteEmployee() {
+        CreateInstance();
+        Call<Void> voidCall=employeeApi.deleteEmployee(Integer.parseInt(etEmployeeNo.getText().toString()));
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(UpdateDeleteActivity.this, "Sucessfully deleted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(UpdateDeleteActivity.this, "Error"+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateEmployee() {
+        EmployeeCUD employeeCUD=new EmployeeCUD(
+                etUpdateName.getText().toString(),
+                Float.parseFloat(etUpdateSalary.getText().toString()),
+                Integer.parseInt(etUpdateAge.getText().toString())
+        );
+        Call<Void> voidCall=employeeApi.updateEmployee(Integer.parseInt(etEmployeeNo.getText().toString()),employeeCUD);
+
+        voidCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(UpdateDeleteActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(UpdateDeleteActivity.this, "Error"+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadData() {
+        if (TextUtils.isEmpty(etEmployeeNo.getText())){
+            etEmployeeNo.setError("Please enter employee id");
+            return;
+        }
+        CreateInstance();
+       Call<Employee> listCall=employeeApi.getEmployeeByID(Integer.parseInt(etEmployeeNo.getText().toString()));
+
+       listCall.enqueue(new Callback<Employee>() {
+           @Override
+           public void onResponse(Call<Employee> call, Response<Employee> response) {
+               etUpdateName.setText(response.body().getEmployee_name());
+               etUpdateSalary.setText(response.body().getEmployee_salary());
+               etUpdateAge.setText(Integer.toString(response.body().getEmployee_age()));
+           }
+
+           @Override
+           public void onFailure(Call<Employee> call, Throwable t) {
+               Toast.makeText(UpdateDeleteActivity.this, "Error"+t.getMessage(), Toast.LENGTH_SHORT).show();
+           }
+       });
+    }
+
+    private void CreateInstance() {
+        retrofit=new Retrofit.Builder()
+                .baseUrl(Url.base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        employeeApi=retrofit.create(EmployeeApi.class);
     }
 }
--- app/src/main/java/com/pawan/employeeapi/api/EmployeeApi.java
+++ app/src/main/java/com/pawan/employeeapi/api/EmployeeApi.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface EmployeeApi {
@@ -21,4 +23,12 @@ public interface EmployeeApi {
 
     @POST("create")
     Call<Void>registerEmploee(@Body EmployeeCUD emp);
+
+    //Delete Employee on the basis of EmpID
+    @DELETE("delete/{empID}")
+    Call<Void> deleteEmployee(@Path("empID") int empID);
+
+    //Update Employee on the basis of EmpID
+    @PUT("update/{empID}")
+    Call<Void> updateEmployee(@Path("empID") int empID, @Body EmployeeCUD emp);
 }
